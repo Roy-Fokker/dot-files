f4d2721f0d21e318c9532dd45e9511b0895fb9f2
Neel Raiyani <nraiyani@gmail.com>
2019-02-21 17:31:24 -0500

    Update GC threshold values, change gc-threshold and file-handler reset

--- init.el
+++ init.el
@@ -1,40 +1,19 @@
 (eval-and-compile
   ;; Change GC threshold for duration of init, technique from DOOM Emacs FAQ
   ;; Reset at the end of this file
-  (setq gc-cons-threshold 402653184
+  (setq gc-cons-threshold 2147483648 ; set gc threshold to 2GiB
         gc-cons-percentage 0.6))
 
 ;; file handler check is not needed during start up
 ;; reset at the end of this file
-(defvar temp--file-name-handler-alist file-name-handler-alist)
+(defvar startup/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-
-;; Some defaults for emacs
-(setq-default inhibit-startup-message t    ; Disable emacs welcome screen
-              indent-tabs-mode nil         ; Don't use hard tabs
-              ) 
-              
-
-(setq backup-directory-alist `(("." . "backups")) ; backup files in this directory
-      auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) ; transform backup file names
-      package-enable-at-startup nil ; do not load packages before start up
-      delete-by-moving-to-trash t  ; delete moves to recycle bin
-      version-control nil          ; disable emacs version control
-      column-number-mode t         ; display column number
-      show-paren-delay 0           ; show matching immediately
-      )
-
-(set-language-environment "UTF-8")  ; Use UTF-8
-(set-default-coding-systems 'utf-8) ; Use UTF-8
-(prefer-coding-system 'utf-8)       ; Use UTF-8
-(global-display-line-numbers-mode)  ; Display line-numbers in all buffers
-(global-hl-line-mode)               ; Highlight current line
-(menu-bar-mode -1)                  ; Hide menu bar
-(tool-bar-mode -1)                  ; Hide tool bar
-(scroll-bar-mode -1)                ; Hide scroll bar
-(show-paren-mode t)                 ; Parenthesis highlighting
-(delete-selection-mode t)           ; Enable delete selection mode
+(defun startup/reset-gc-and-file-handler ()
+  (setq gc-cons-threshold 536870912  ; set gc threshold to 0.5GiB
+        gc-cons-percentage 0.1
+        file-name-handler-alist startup/file-name-handler-alist)
+  )
 
 
 (eval-when-compile
@@ -184,11 +163,7 @@
 ;; Reset changed values to defaults
 ;; values taken from DOOM-Emacs FAQ
 (eval-and-compile
-  (add-hook 'emacs-startup-hook
-            '(lambda ()
-               (setq gc-cons-threshold 16777216
-                     gc-cons-percentage 0.1
-                     file-name-handler-alist temp--file-name-handler-alist))))
+  (add-hook 'emacs-startup-hook 'startup/reset-gc-and-file-handler))
 
 (setq initial-scratch-message (concat "Startup time: " (emacs-init-time)))
 (provide 'init)
