43d1013721f4aaf26cf0f9a9345a1a6febc47f17
Neel Raiyani <nraiyani@gmail.com>
2019-06-20 19:11:10 -0400

    Use MELPA with http instead of HTTPS

--- init.el
+++ init.el
@@ -116,7 +116,7 @@
   ;; Where to look for packages
   (add-to-list 'package-archives '("org"   . "http://orgmode.org/elpa/") t)
   (add-to-list 'package-archives '("elpa"  . "http://elpa.gnu.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
   ;; initialize package
   (package-initialize))
