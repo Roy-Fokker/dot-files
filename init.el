465f479244437f5028312704f4d56573fd4822e4
Neel Raiyani <nraiyani@gmail.com>
2019-08-19 21:47:24 -0400

    Add notes-style.org

--- init.el
+++ init.el
@@ -334,7 +334,9 @@
     ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
  '(package-selected-packages
    (quote
-    ()))
+    (yasnippet-snippets winum which-key slime-company rainbow-delimiters paredit magit ivy-rich geiser eldoc-overlay dracula-theme doom-modeline counsel company-quickhelp common-lisp-snippets all-the-icons-ivy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
