7f08970c9edbb1d8fc0ba428c947a3209d89a969
Neel Raiyani <nraiyani@gmail.com>
2020-03-24 17:18:51 -0400

    Force rainbow delimiters to use more vibrant colors

--- init.el
+++ init.el
@@ -310,7 +310,16 @@
 	  slime-repl-mode
 	  cider-repl-mode
 	  racket-repl-mode)
-	 . rainbow-delimiters-mode))
+	 . rainbow-delimiters-mode)
+  :custom-face
+  (rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+  (rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+  (rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+  (rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+  (rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+  (rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+  (rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+  (rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
 
 ;; - Treemacs -------------------------------------------------------
 (use-package treemacs
